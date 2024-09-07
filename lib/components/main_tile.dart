e4ed7fb4b42c234ea0c915d67400186b3514ee05
jydv402 <jydv402@gmail.com>
2024-09-07 13:33:04 +0530

    Added list filterer

--- lib/components/main_tile.dart
+++ lib/components/main_tile.dart
@@ -4,8 +4,17 @@ import 'package:memno/functionality/code_gen.dart';
 import 'package:memno/theme/app_colors.dart';
 import 'package:provider/provider.dart';
 
-class MainTile extends StatelessWidget {
+enum Filters { all, liked, empty }
+
+class MainTile extends StatefulWidget {
   const MainTile({super.key});
+
+  @override
+  State<MainTile> createState() => _MainTileState();
+}
+
+class _MainTileState extends State<MainTile> {
+  Filters _filter = Filters.all;
 
   @override
   Widget build(BuildContext context) {
@@ -15,29 +24,109 @@ class MainTile extends StatelessWidget {
         if (!codeProvider.isReady) {
           return const Center(child: CircularProgressIndicator());
         }
-        return codeProvider.codeList.isEmpty
-            ? const Center(
-                child: Text(
-                  "Generate Code to view",
-                ),
+
+        final filteredList = listFilter(codeProvider);
+
+        return filteredList.isEmpty
+            ? ListView(
+                children: [
+                  TopAccentBox(
+                    colors: colors,
+                    length: filteredList.length,
+                    filter: _filter,
+                    customToggle: _customToggleButtons(context),
+                  ),
+                  const SizedBox(height: 100),
+                  Center(
+                    child: Text(
+                      _emptyMsg(),
+                      style: TextStyle(
+                        color: colors.textClr,
+                        fontFamily: 'Product',
+                      ),
+                    ),
+                  ),
+                ],
               )
             : ListView.builder(
                 padding: const EdgeInsets.only(bottom: 130),
-                itemCount: codeProvider.codeList.length + 1,
+                itemCount: filteredList.length + 1,
                 itemBuilder: (context, index) {
                   if (index == 0) {
                     return TopAccentBox(
                       colors: colors,
-                      length: codeProvider.codeList.length,
+                      length: filteredList.length,
+                      filter: _filter,
+                      customToggle: _customToggleButtons(context),
                     );
                   } else {
-                    final code = codeProvider.codeList[index - 1];
+                    final code = filteredList[index - 1];
                     final date = codeProvider.getDateForCode(code);
                     final isLiked = codeProvider.getLikeForCode(code);
                     return subTile(context, code, date, isLiked);
                   }
                 });
       },
+    );
+  }
+
+  List<int> listFilter(CodeGen codeProvider) {
+    switch (_filter) {
+      case Filters.all:
+        return codeProvider.codeList;
+      case Filters.liked:
+        return codeProvider.codeList
+            .where((element) => codeProvider.getLikeForCode(element))
+            .toList();
+      case Filters.empty:
+        //where
+        return codeProvider.codeList
+            .where((element) => codeProvider.getLinkListLength(element) == 0)
+            .toList();
+      default:
+        return codeProvider.codeList;
+    }
+  }
+
+  String _emptyMsg() {
+    switch (_filter) {
+      case Filters.all:
+        return "Generate Code to view";
+      case Filters.liked:
+        return "No liked codes";
+      case Filters.empty:
+        return "No empty codes";
+      default:
+        return "Generate Code to view";
+    }
+  }
+
+  Widget _customToggleButtons(BuildContext context) {
+    final colors = Provider.of<AppColors>(context);
+    return ToggleButtons(
+      borderColor: Colors.black,
+      selectedBorderColor: Colors.black,
+      selectedColor: colors.accntText,
+      fillColor: colors.accntPill,
+      direction: Axis.horizontal,
+      onPressed: (int index) {
+        setState(() {
+          _filter = Filters.values[index];
+        });
+      },
+      borderRadius: BorderRadius.circular(50),
+      isSelected: Filters.values.map((e) => e == _filter).toList(),
+      children: const [
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 19, vertical: 19),
+            child: Text('   All   ')),
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 19, vertical: 19),
+            child: Text('Liked')),
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 19, vertical: 19),
+            child: Text('Empty')),
+      ],
     );
   }
 }
@@ -47,10 +136,14 @@ class TopAccentBox extends StatelessWidget {
     super.key,
     required this.colors,
     required this.length,
+    required this.filter,
+    required this.customToggle,
   });
 
   final AppColors colors;
   final int length;
+  final Filters filter;
+  final Widget customToggle;
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +151,7 @@ class TopAccentBox extends StatelessWidget {
       padding: const EdgeInsets.fromLTRB(2, 0, 2, 4),
       child: Stack(
         children: [
+          //Main accent container
           Container(
             height: 300,
             decoration: BoxDecoration(
@@ -65,14 +159,22 @@ class TopAccentBox extends StatelessWidget {
               color: colors.accnt,
             ),
           ),
+          //Custom toggle button
+          Positioned(
+            bottom: 16,
+            left: 16,
+            child: customToggle,
+          ),
+          //Intro text
           const Positioned(
-              top: 26,
+              top: 52,
               left: 26,
               child: Text("Hi,\nI'm Memno",
                   style: TextStyle(
                       fontFamily: 'Product',
                       fontWeight: FontWeight.w700,
                       fontSize: 52))),
+          //Code count
           Positioned(
             bottom: 16,
             right: 16,
@@ -80,6 +182,7 @@ class TopAccentBox extends StatelessWidget {
               width: 130,
               padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
                 color: colors.accntPill,
                 borderRadius: BorderRadius.circular(50),
               ),
