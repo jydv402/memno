b85be303c78db3a6df63fb44a872026415351407
jydv402 <jydv402@gmail.com>
2026-06-18 01:32:54 +0530

    Update packages

--- lib/functionality/check_update.dart
+++ lib/functionality/check_update.dart
@@ -10,45 +10,39 @@ import 'package:memno/theme/app_colors.dart';
 Future<void> checkAppUpdate(BuildContext context, bool showIfUpToDate) async {
   final colors = Provider.of<AppColors>(context, listen: false);
 
-  final config = UpdateCheckerConfig(
-    githubRepo: "jydv402/memno",
-    bottomSheetColors: UpdateBottomSheetColors(
-      backgroundColor: colors.isDarkMode ? Colors.grey[900] : Colors.white,
-      textColor: colors.textClr,
-      secondaryTextColor: colors.textClr.withValues(alpha: 0.6),
-      accentColor: colors.accnt,
-      accentTextColor: Colors.black,
-      pillColor: colors.pill,
-      boxColor: colors.box,
-    ),
-    bottomSheetStyles: UpdateBottomSheetStyles(
-      borderRadius: 35.0,
-      buttonBorderRadius: 50.0,
-      titleStyle: TextStyle(
-        fontFamily: 'GoogleSans',
-        fontWeight: FontWeight.bold,
-        color: colors.textClr,
-        fontSize: 32,
-      ),
-      versionStyle: TextStyle(fontFamily: 'GoogleSans', color: colors.textClr),
-      whatsNewStyle: TextStyle(
-        fontFamily: 'GoogleSans',
-        fontWeight: FontWeight.w600,
-        color: colors.textClr,
-        fontSize: 24,
-      ),
-      contentStyle: TextStyle(fontFamily: 'GoogleSans', color: colors.textClr),
-      buttonTextStyle: TextStyle(
-        fontFamily: 'GoogleSans',
-        fontWeight: FontWeight.w600,
-        fontSize: 18,
-      ),
-    ),
-  );
-
-  await UpdateCheckerBottomSheet.checkAndUpdate(
+  await UpdateChecker.check(
     context,
-    config: config,
+    githubRepo: "jydv402/memno",
     showIfUpToDate: showIfUpToDate,
+    backgroundColor: colors.isDarkMode ? Colors.grey[900] : Colors.white,
+    textColor: colors.textClr,
+    secondaryTextColor: colors.textClr.withValues(alpha: 0.6),
+    accentColor: colors.accnt,
+    accentTextColor: Colors.black,
+    pillColor: colors.pill,
+    boxColor: colors.box,
+    borderRadius: 35.0,
+    buttonBorderRadius: 50.0,
+    titleStyle: TextStyle(
+      fontFamily: 'GoogleSans',
+      fontWeight: FontWeight.bold,
+      color: colors.textClr,
+      fontSize: 32,
+    ),
+    versionStyle: TextStyle(fontFamily: 'GoogleSans', color: colors.textClr),
+    whatsNewStyle: TextStyle(
+      fontFamily: 'GoogleSans',
+      fontWeight: FontWeight.w600,
+      color: colors.textClr,
+      fontSize: 24,
+    ),
+    contentStyle: TextStyle(fontFamily: 'GoogleSans', color: colors.textClr),
+    buttonTextStyle: TextStyle(
+      fontFamily: 'GoogleSans',
+      fontWeight: FontWeight.w600,
+      fontSize: 18,
+    ),
+    showBorder: true,
+    showHandle: true,
   );
 }
